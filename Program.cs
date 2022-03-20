370c17d400a8c2c6484fdecbf303070721df571c
= <=>
2022-03-20 19:42:17 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//CRIA VARIAVEIS
+
+string nome, sobrenome;
+
+Console.Write("Digite seu primeiro nome:");
+nome = Console.ReadLine();
+
+Console.Write("Digite seu sobrenome....:");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"Nome completo...: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()},{nome}");
+
+
+
+
+
+
